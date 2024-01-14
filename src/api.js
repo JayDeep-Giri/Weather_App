2d3c808adfc7ba6abe796b8bd47576f23b83692a
JayDeep-Giri <jaydeepgiri30@gmail.com>
2024-01-14 21:56:09 +0530

    Api data and options

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoUrl = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'use your api',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+export const Weather_Api_url = "https://api.openweathermap.org/data/2.5";
+export const Weather_api_key = "use your api ";
+
+
+
